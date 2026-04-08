7ed47ecd0060a13806cdd0c0567a43400c6399d4
Jiya Kothari <147786343+Jiya2406@users.noreply.github.com>
2026-04-08 18:32:44 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalendarApp from './components/CalendarApp';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <CalendarApp />
+    </>
+  );
+}
+
+export default App;
